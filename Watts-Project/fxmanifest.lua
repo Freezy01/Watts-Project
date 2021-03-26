50dacf8ac2080de9e8774bf6e1ef8fa6bec48b1e
Freezy01 <76497290+Freezy01@users.noreply.github.com>
2021-03-26 15:12:34 -0700

    Add files via upload

---
+++ Watts-Project/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Freezy'
+description 'Watts Project'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+
+client_script '@AntiCheat/xDxDxDxDxD.lua'
+
+client_script '@ls-studio/xDxDxDxDxD.lua'
